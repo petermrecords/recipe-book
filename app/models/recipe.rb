0dd5bf0229272fd6852fa3c4dd8a957cec17f584
BeggarsPeterM <petermenniti@beggars.com>
2019-02-10 21:57:55 -0500

    new recipe page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ApplicationRecord
 	validates :dish_name, { presence: true, uniqueness: true }
-	validates :admin, { presence: true }
+	validates :author, { presence: true }
+	validates :serves, { presence: true, numericality: {
+		greater_than_or_equal_to: 1,
+		less_than_or_equal_to: 12
+	} }
 
 	belongs_to :author, class_name: 'Admin', foreign_key: :admin_id
 
