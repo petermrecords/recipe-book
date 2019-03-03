d69c6af86a6f79cb105e08c76d4b91d08c7f8f0a
BeggarsPeterM <petermenniti@beggars.com>
2019-03-03 11:32:54 -0500

    publish buttons

--- db/seeds/super_admin.seeds.rb
+++ db/seeds/super_admin.seeds.rb
@@ -1,21 +1,25 @@
 super_admin = Admin.find_by(email: ENV['SUPER_ADMIN_EMAIL_ADDRESS'])
 
 if super_admin
-	puts "Super admin account found."
+	puts "Super admin account is registered to #{super_admin.email}."
+elsif !ENV['SUPER_ADMIN_EMAIL_ADDRESS']
+  raise ArgumentError, "An email address for the super admin has not been provided - please add one to the environment prior to setup."
 else
-	super_admin = Admin.new({
-		first_name: "Super", 
-		last_name: "Admin", 
-		email: ENV['SUPER_ADMIN_EMAIL_ADDRESS']
-	})
-  puts "Enter a super admin password:"
-  super_admin.password = $stdin.gets.chomp
-  puts "Confirm the super admin password:"
-  super_admin.password_confirmation = $stdin.gets.chomp
-  if super_admin.save
-	 puts "Super admin account created for #{ENV['SUPER_ADMIN_EMAIL_ADDRESS']}."
-  else
-    puts "Errors prevented the super admin account from being saved:"
-    super_admin.errors.full_messages.each { |error| puts error }
+  while !(super_admin && super_admin.valid?)
+  	super_admin = Admin.new({
+  		first_name: "Super", 
+  		last_name: "Admin", 
+  		email: ENV['SUPER_ADMIN_EMAIL_ADDRESS']
+  	})
+    puts "Enter a super admin password:"
+    super_admin.password = $stdin.gets.chomp
+    puts "Confirm the super admin password:"
+    super_admin.password_confirmation = $stdin.gets.chomp
+    if super_admin.save
+  	 puts "Super admin account created for #{ENV['SUPER_ADMIN_EMAIL_ADDRESS']}."
+    else
+      puts "Errors prevented the super admin account from being saved:"
+      super_admin.errors.full_messages.each { |error| puts error }
+    end
   end
 end
